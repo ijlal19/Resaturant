a6d5f4169b2ff3c9d62a2c96cdb75db26c69f5f0
abdullah30nov <abdullahbinaslam64@gmail.com>
2025-11-11 03:41:21 +0500

    Update

---
+++ src/website/sharedComponent/header/data.tsx
@@ -0,0 +1,21 @@
+const HeaderBarData = [
+    {
+        messgae: " 11.11 Mega Deal: Buy 2 Get 1 FREE!",
+        color: "purple",
+        toggle: true,
+        rotation : 1500
+    },
+    {
+        messgae: " 11.11 Mega Deal: Buy 2 Get 1 FREE!",
+        color: "brown",
+        toggle: true,
+        rotation : 1000
+    },
+    {
+        messgae: " 11.11 Mega Deal: Buy 2 Get 1 FREE!",
+        color: "grey",
+        toggle: true,
+        rotation : 500
+    }
+]
+export default HeaderBarData
